d27273153e7d054445a5835fbd7799d0b5c66607
Leony Tamio Hatakeyama <leonytamio@gmail.com>
2025-03-31 23:29:08 -0400

    DockerFile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { PessoaListaComponent } from './pages/pessoa-lista/pessoa-lista.component';
 import { PessoaDetalheComponent } from './pages/pessoa-detalhe/pessoa-detalhe.component';
-import { PessoaInfoFormComponent } from './pages/pessoa-info-form/pessoa-info-form.component';
+
 
 export const routes: Routes = [
   { path: '', component: PessoaListaComponent },
   { path: 'pessoa/:id', component: PessoaDetalheComponent },
-  { path: 'pessoa/:id/info', component: PessoaInfoFormComponent }
 ];
